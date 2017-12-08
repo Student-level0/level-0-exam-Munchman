dcd493c88e05e6000b68850daa6666b0b90bb40e
DemoliciousDt <league@Thes-Mac-mini.local>
2017-12-07 17:42:08 -0800

    finish

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -4,16 +4,28 @@ import org.jointheleague.graphical.robot.Robot;
 
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
-
+  
     public static void main(String[] args) {
+   	 Robot james=new Robot();
+   	 james.penDown();
    	 // 3. ask the user what color they would like the Robot to draw
-
+String color =JOptionPane.showInputDialog(null, "what color do you want your square? Blue or Red? No caps please!");
    	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
 
-   	 // 2. set the pen width to 10
+if(color=="blue"){
+	james.setPenColor(25,30,150);
+}
+else if(color=="red"){
+	james.setPenColor(150,20,60);
+}
 
+   	 // 2. set the pen width to 10
+james.setPenWidth(10);
    	 // 1. make the Robot draw a shape
-   	 
+for (int i = 0; i < 4; i++) {
+	james.move(100);
+	james.turn(90);
+}
     }
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -10,7 +10,16 @@ import javax.swing.JOptionPane;
  **/
 
 public class CodingExercise2 {
-
+public static void main(String[] args) {
+	String age =JOptionPane.showInputDialog("how old are you?");
+	int a =Integer.parseInt(age);
+	if (a>=30){
+		JOptionPane.showMessageDialog(null, "YOUR TOO OLD!!!");
+	}
+	if (a<=29){
+		JOptionPane.showMessageDialog(null, 2017-a);
+	}
+}
 }
 
 
